7d4d4c1bcb6ae1854f69b529f6f6d8f28be95e4a
Ashraf <z1ashraf@ieng6-202.ucsd.edu>
2025-11-19 20:12:16 -0800

    malloc and free implemented

--- vmalloc.c
+++ vmalloc.c
@@ -1,8 +1,80 @@
 #include "vm.h"
 #include "vmlib.h"
 
+size_t get_curr_status(struct block_header* block) {
+    if((block->size_status & 1) == 1) { return 1; }
+    return 0;
+}
+
+size_t get_prev_status(struct block_header* block) {
+    if((block->size_status & 0x2) == 2) { return 1; }
+    return 0;
+}
+
+size_t get_block_size(struct block_header* block) {
+ size_t size = block->size_status & ~0xF;
+
+ return size;
+}
+
+struct block_header* next_block_addr(struct block_header* start){
+    size_t size = get_block_size(start);
+    struct block_header* ptr = (struct block_header*)((char*)start + size);
+    return ptr;
+}
+
+size_t min_block_size(size_t bytes) {
+    size_t size = 0;
+    if(bytes == 0) { return size; }
+
+    size = bytes + sizeof(size_t);
+    while(size % 16 != 0) {
+     size += 1;
+    }
+
+    return size;
+}
+
 void *vmalloc(size_t size)
 {
-    // TODO
-    return NULL;
+    if(size == 0) { return NULL; }
+
+    size_t required_size = min_block_size(size);
+
+	struct block_header* curr = heapstart;
+    struct block_header* best = NULL;
+	size_t best_size = SIZE_MAX;
+
+	while(get_block_size(curr) != 0) {
+     if(get_curr_status(curr) == 0) {
+      size_t curr_size = get_block_size(curr);
+	  if(curr_size >= required_size && curr_size < best_size) {
+       best = curr;
+	   best_size = curr_size;
+	  }
+	 }
+	 curr = next_block_addr(curr);
+	}
+
+	if(best == NULL) { return NULL; }
+
+	size_t leftover_bytes = best_size - required_size;
+
+	if(leftover_bytes >= 16) {
+     struct block_header* new_block = (struct block_header*)((char*)best + required_size);
+     new_block->size_status = leftover_bytes | 0x2;
+     
+	 struct block_footer* footer = (struct block_footer*)((char*)new_block + leftover_bytes - sizeof(size_t));
+	 footer->size = leftover_bytes;
+
+	 best->size_status = required_size | (best->size_status & 0x2);
+	}
+	else {
+	 struct block_header* next = next_block_addr(best);
+	 next->size_status |= 0x2;
+	}
+
+	best->size_status |= 0x1;
+	return (void*)((char*)best + sizeof(struct block_header));
+
 }
--- vmfree.c
+++ vmfree.c
@@ -1,6 +1,27 @@
 #include "vm.h"
 #include "vmlib.h"
 
+size_t get_curr_status(struct block_header* block) {
+     if((block->size_status & 1) == 1) { return 1; }
+     return 0;
+}
+
+size_t get_prev_status(struct block_header* block) {
+      if((block->size_status & 0x2) == 2) { return 1; }
+      return 0;
+}
+
+size_t get_block_size(struct block_header* block) {
+   size_t size = block->size_status & ~0xF;
+  
+   return size;
+}
+
+struct block_header* next_block_addr(struct block_header* start) {
+        size_t size = get_block_size(start);
+        struct block_header* ptr = (struct block_header*)((char*)start + size);
+        return ptr;
+}
 /**
  * The vmfree() function frees the memory space pointed to by ptr,
  * which must have been returned by a previous call to vmmalloc().
@@ -10,5 +31,38 @@
  */
 void vmfree(void *ptr)
 {
-    // TODO
+    if(ptr == NULL) { return; }
+
+	struct block_header* block = (struct block_header*)((char*)ptr - sizeof(struct block_header));
+	if(get_curr_status(block) == 0) { return; }
+
+	block->size_status &= ~0x1;
+	size_t block_size = get_block_size(block);
+
+	struct block_footer* footer = (struct block_footer*)((char*)block + block_size - sizeof(struct block_footer));
+	footer->size = block_size;
+
+	struct block_header* next = next_block_addr(block);
+	if(get_curr_status(next) == 0) {
+     size_t new_size = block_size + get_block_size(next);
+	 block->size_status = new_size | (block->size_status & 0x2);
+	 struct block_footer* new_footer = (struct block_footer*)((char*)block + new_size - sizeof(struct block_footer));
+	 new_footer->size = new_size;
+	 block_size = new_size;
+	}
+
+	if(get_prev_status(block) == 0 && block != heapstart) {
+     struct block_footer* prev_footer = (struct block_footer*)((char*)block - sizeof(struct block_footer));
+     struct block_header* prev_header = (struct block_header*)((char*)block - prev_footer->size);
+     
+	 size_t new_size = get_block_size(prev_header) + get_block_size(block);
+	 prev_header->size_status = new_size | (prev_header->size_status & 0x2);
+	 struct block_footer* footer = (struct block_footer*)((char*)prev_header + new_size - sizeof(struct block_footer));
+	 footer->size = new_size;
+	 block = prev_header;
+	 block_size = new_size;
+	}
+
+	struct block_header* next_block = next_block_addr(block);
+	next_block->size_status &= ~0x2;
 }
